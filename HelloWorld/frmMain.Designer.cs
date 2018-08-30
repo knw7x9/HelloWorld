5ab0969973afa65072e70803439c519073d681ee
Katherine Wilsdon <katherinewilsdon@gmail.com>
2018-08-29 21:19:48 -0600

    HelloWorld
    Produces a Message Box that displays Hello {Entry in the textbox}!.

---
+++ HelloWorld/frmMain.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace HelloWorld {
+    partial class frmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(36, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(204, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.Text = "World";
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.Location = new System.Drawing.Point(36, 114);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(75, 23);
+            this.btnSayHello.TabIndex = 1;
+            this.btnSayHello.Text = "Say Hello";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(165, 114);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 172);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSayHello);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmMain";
+            this.Text = "Hello App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ HelloWorld/frmMain.cs
@@ -0,0 +1,35 @@
+﻿// frmMain.cs
+// CS 1181
+// Katherine Wilsdon
+// 29 August 2018
+// Dr. Cody Permann
+// Description - Produces a Message Box that displays Hello {Entry in the textbox}!.
+// WOW: Click the Exit button to close Hello App.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld {
+    public partial class frmMain : Form {
+        public frmMain() {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, EventArgs e) {
+            // Produces a Message Box that displays Hello {Entry in the textbox}!.
+            MessageBox.Show("Hello " + txtName.Text + "!");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            // Close Hello App.
+            this.Close();
+        }
+    }
+}
